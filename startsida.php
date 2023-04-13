97391f74c7a60c38312756e1445e1af87d07e920
Hugge05 <75117937+Hugge05@users.noreply.github.com>
2023-04-13 11:05:50 +0200

    börjat med startsidan och ska börja med att göra log in nästa lektion

--- startsida.php
+++ startsida.php
@@ -1,22 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="Styling.css">  
+<title> Flutter </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php 
 
 ?>
-<head>
-    <link rel="stylesheet" href="styling.css">  
-</head>
+
 
 <div class="LogoText">
-    <a href="startsida.php"> Flutter </a>
+    <a href="startsida.php" class="LogoText"> Flutter </a>
 
 
 </div>
 <body>
 
 <form action="" method="POST">
-    
-<input 
-</form>
-</body>
-<?php
 
+<!--<input type="text" name="t1" class="Flutt" placeholder="Make a noise"> -->
+</form>
+ 
+    <button class="loggainKnapp" stlye="width:100px;">
+    <a href="loggain.php"> hej</a>
+    </button>
+<?php
+#$Flutt = $_POST["t1"];
+
+/*if(strlen($Flutt) == 0)
+{
+    ?>
+    <script>
+        alert("Du måste ha skrivit något för att skicka!");
+        </script>
+        <?php
+}*/
 ?>
+</body>
+</html>
